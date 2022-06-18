2adf5b38825a49d6fbc71b1a5ec7a994f726fcb5
AnaLuMarzo <78816332+AnaLuMarzo@users.noreply.github.com>
2022-06-17 21:03:15 -0300

    Update randomNumber.s

--- randomNumber.s
+++ randomNumber.s
@@ -4,6 +4,7 @@
 .stack 100h
 
 .data 
+  outputMsg db "Random Number between (0-9): $"  #outputMsg is used for output this string on console
   randomNum db 0   # salvo nuestro num random 
   
 main:
@@ -11,12 +12,21 @@ main:
     mov ds, ax
     
     mov ah, 09h
-    mov dx, offset
-    int 21h    #
+    mov dx, offset outputMsg
+    int 21h    # this will display output msg
     
+    call generateRandomNumber   #llamo a la subrutina random
+    
+    mov ah, 02h  
+    mov dl, randomNum   #our result is 0 to 9, it will be save in dl
+    add dl, '0'   # add 48 decimal value in dl, so, number will display on screen
+    int 21h
+    
+    mov ah, 4ch   #exit point of our program
+    int 21h
   
 generateRandomNumber:
-     mov ah, 0   #interrupo para tener el tiempo del sistema
+     mov ah, 0h   #interrupo para tener el tiempo del sistema
      int 1ah     #ahora el numero del reloj se salva en DX
      
      mov ax,dx   #muevo el numero de las agujas en ax
